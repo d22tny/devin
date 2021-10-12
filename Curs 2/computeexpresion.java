c21f452308b554926b68e6eaae6bd4e480d53f77
d22tny <d22tny@gmail.com>
2021-10-12 16:19:20 +0300

    computeexpression

---
+++ Curs 2/computeexpresion.java
@@ -0,0 +1,16 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+    
+}
+

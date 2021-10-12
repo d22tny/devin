3c93da320089ce5ce6844e1ac59f296ee2298e26
d22tny <d22tny@gmail.com>
2021-10-12 16:16:45 +0300

    Welcome

---
+++ Curs 2/welcome.java
@@ -0,0 +1,16 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Java!") ;
+    }
+    
+}
+

24c3d414f5b52aa3fc6f9475917cf5cf353fd2c2
d22tny <d22tny@gmail.com>
2021-10-12 17:47:56 +0300

    problema 12 chapter 1

---
+++ Curs 2/problema12chpater1.java
@@ -0,0 +1,18 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        double km = 24 * 1.6;
+        double timp = 3600 + 40 * 60 + 35;
+        System.out.println((km * 3600)/timp);
+    }
+    
+}
+

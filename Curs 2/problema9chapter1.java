a2138b195eec14b273d712f5a1c341a03e981d09
d22tny <d22tny@gmail.com>
2021-10-12 17:05:31 +0300

    problema 9 chpater 1

---
+++ Curs 2/problema9chapter1.java
@@ -0,0 +1,18 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        double width = 4.5;
+        double height = 7.9;
+        System.out.println("Aria este: " + width*height);
+    }
+    
+}
+

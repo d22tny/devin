68e3baee53d7a7756bc50a82f366608abb92ca33
d22tny <d22tny@gmail.com>
2021-10-12 16:55:34 +0300

    problema 7 chpater 1

---
+++ Curs 2/problema7chapter1.java
@@ -0,0 +1,17 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        double x = 1.0 - (double)1/3 + (double)1/5 - (double)1/7 + (double)1/9 - (double)1/11;
+        System.out.println(x);
+    }
+    
+}
+

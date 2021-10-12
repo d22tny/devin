5d870f35221d9bdfe846efde8a6851fa105a222e
d22tny <d22tny@gmail.com>
2021-10-12 17:03:31 +0300

    problema 8 chapter 1

---
+++ Curs 2/problema8chapter1.java
@@ -0,0 +1,20 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        double pi = 3.1415926535;
+        double perimeter = 2 * 5.5 * pi;
+        double area = 5.5 * 5.5 * pi;
+        System.out.println("Aria este: " + area);
+        System.out.println("Perimetrul este: " + perimeter);
+    }
+    
+}
+

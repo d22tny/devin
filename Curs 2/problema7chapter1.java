2e4617ec4a0051d945c89041325980d6f5bb4e61
d22tny <d22tny@gmail.com>
2021-10-12 16:58:15 +0300

    a

--- Curs 2/problema7chapter1.java
+++ Curs 2/problema7chapter1.java
@@ -10,7 +10,9 @@ public class Devin {
     public static void main(String args[])
     {
         double x = 1.0 - (double)1/3 + (double)1/5 - (double)1/7 + (double)1/9 - (double)1/11;
-        System.out.println(x);
+        double y = 1.0 - (double)1/3 + (double)1/5 - (double)1/7 + (double)1/9 - (double)1/11 + (double)1/13;
+        System.out.println((double)4*x);
+        System.out.println((double)4*y);
     }
     
 }

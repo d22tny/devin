9bdd4974727b598e43ef46cf5e604b2a615f36c6
d22tny <d22tny@gmail.com>
2021-10-12 16:31:30 +0300

    problema 3 chapter 1

---
+++ Curs 2/problema3chapter1.java
@@ -0,0 +1,19 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        System.out.println("    J     A     V     V    A");
+        System.out.println("    J    A A     V   V    A A");
+        System.out.println("J   J   AAAAA     V V    AAAAA");
+        System.out.println(" J J   A     A     V    A     A");
+    }
+    
+}
+

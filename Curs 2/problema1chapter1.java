1267cf4780c0b7e5a45316c2379f1980d18b2ee0
d22tny <d22tny@gmail.com>
2021-10-12 16:26:45 +0300

    problema 1 capitol 1

--- Curs 2/problema1chapter1.java
+++ Curs 2/problema1chapter1.java
@@ -9,9 +9,7 @@ public class Devin {
      
     public static void main(String args[])
     {
-        for (int i = 1; i<=5; i++) {
-            System.out.println("Welcome to Java!");
-        }
+        System.out.println("Welcome to Java!");
     }
     
 }

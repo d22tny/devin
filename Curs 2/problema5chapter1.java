80f233b57f32851b83bdd379a2488a6c86ff4614
d22tny <d22tny@gmail.com>
2021-10-12 16:56:40 +0300

    a

--- Curs 2/problema5chapter1.java
+++ Curs 2/problema5chapter1.java
@@ -9,8 +9,7 @@ public class Devin {
      
     public static void main(String args[])
     {
-        System.out.println(((9.5*4.5) - (2.5 * 3)) / (45.5 - 3.5));
-       
+        System.out.println((double)(((double)9.5*4.5) - ((double)2.5 * 3)) / ((double)45.5 - 3.5));
     }
     
 }

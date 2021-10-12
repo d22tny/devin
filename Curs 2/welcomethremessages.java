1fb70ebd653a115d1108cb45de503045e2ed3720
d22tny <d22tny@gmail.com>
2021-10-12 16:18:04 +0300

    welcomethreemesages

---
+++ Curs 2/welcomethremessages.java
@@ -0,0 +1,18 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        System.out.println("Programming is fun!") ;
+        System.out.println("Fundamentals first") ;
+        System.out.println("Problem driven") ;
+    }
+    
+}
+

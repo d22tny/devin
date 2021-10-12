3f80cb76287e7f9acc4f65b0da045ce9ef5a8353
d22tny <d22tny@gmail.com>
2021-10-12 16:20:55 +0300

    problema 1.32

---
+++ Curs 2/problema132.java
@@ -0,0 +1,17 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        System.out.println("3.5 * 4 / 2 - 2.5 is");
+        System.out.println(3.5 * 4 / 2 - 2.5);
+    }
+    
+}
+

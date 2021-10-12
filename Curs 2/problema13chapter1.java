75e4ebc952fd57897798a3d7284c237f65f3a6cc
d22tny <d22tny@gmail.com>
2021-10-12 17:53:47 +0300

    problema 13 chapter 1

---
+++ Curs 2/problema13chapter1.java
@@ -0,0 +1,22 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        double a = 3.4;
+        double b = 50.2;
+        double c = 2.1;
+        double d = 0.55;
+        double e = 44.5;
+        double f = 5.9;
+        System.out.println("x = " + ((e*d - b*f)/(a*d - b*c)));
+        System.out.println("y = " + ((a*f - e*c)/(a*d - b*c)));
+    }
+    
+}

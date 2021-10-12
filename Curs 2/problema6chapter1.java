9d7e615fcf698eb3065cd2f4b1f951ac39a3c85e
d22tny <d22tny@gmail.com>
2021-10-12 16:47:27 +0300

    problema 6 chapter 1

---
+++ Curs 2/problema6chapter1.java
@@ -0,0 +1,20 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        int sum = 0;
+        for (int i = 1; i<= 9; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+    
+}
+

0c7fef0c761cf4376c9456d75e59811d07f79c89
d22tny <d22tny@gmail.com>
2021-10-12 16:27:10 +0300

    problema 2 capitol 1

---
+++ Curs 2/problema2chapter1.java
@@ -0,0 +1,18 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        for (int i = 1; i<=5; i++) {
+            System.out.println("Welcome to Java!");
+        }
+    }
+    
+}
+

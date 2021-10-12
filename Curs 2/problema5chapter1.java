7fbd22d0f1418663abb664947614fbcce44501e2
d22tny <d22tny@gmail.com>
2021-10-12 16:46:23 +0300

    problema 5 chapter 1

---
+++ Curs 2/problema5chapter1.java
@@ -0,0 +1,17 @@
+package devin;
+import java.util.Scanner;
+/**
+ *
+ * @author d22tny
+ */
+
+public class Devin {
+     
+    public static void main(String args[])
+    {
+        System.out.println(((9.5*4.5) - (2.5 * 3)) / (45.5 - 3.5));
+       
+    }
+    
+}
+
